538293fd2fab2c16976be6f22495d408380e69a6
Mwan3kii <agathawanja303@gmail.com>
2023-10-06 13:39:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	char f;
+
+	c = 'a';
+	f = 'A';
+
+	while
+		(c <= 'a') {
+			putchar(c);
+			c++;
+		}
+	while
+		(f <= 'Z') {
+			putchar(f);
+			f++;
+		}
+	putchar('\n');
+	return (0);
+}
