dd836ed7e73fd33c2d27285c1d4d97d027a67974
Mwan3kii <agathawanja303@gmail.com>
2023-10-12 16:45:02 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,25 +17,16 @@ int main(void)
 
 	printf("%lu", bef);
 
-	for (i = 1; i < 91; i++)
+	for (i = 2; i < 99; i++)
 	{
 		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+		bef1 = (bef / l);
+		bef2 = (bef % l);
+		aft1 = (aft / l);
+		aft2 = (aft % l);
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		aft = aft1 + aft2;
+		bef = bef1 + bef2 + ((aft1 + aft2) / l);
 	}
 	printf("\n");
 	return (0);
