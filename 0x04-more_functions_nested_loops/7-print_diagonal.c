ed7553f52b3a6094607442d3d0275711e7ec25e8
Mwan3kii <agathawanja303@gmail.com>
2023-10-12 13:15:00 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,7 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 0; j < n; j++)
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
@@ -22,6 +22,7 @@ void print_diagonal(int n)
 				else if (j < i)
 					_putchar(' ');
 			}
+			_putchar('\n');
 		}
 	}
 }
