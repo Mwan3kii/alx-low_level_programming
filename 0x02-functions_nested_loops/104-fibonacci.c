fea41eef74952f270ed8c7fc039b623f4bdc0d77
Mwan3kii <agathawanja303@gmail.com>
2023-10-15 14:54:34 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,43 +6,16 @@
  */
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 100000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
-	unsigned long int sum;
+	unsigned long int a = 1, b = 2, temp;
+	int count;
 
-	printf("%lu, %lu", bef, aft);
-
-	for (i = 2; i < 98; i++)
+	printf("%lu, %lu", a, b);
+	for (count = 3; count <= 98; count++)
 	{
-		bef1 = (bef / l);
-		bef2 = (bef % l);
-		aft1 = (aft / l);
-		aft2 = (aft % l);
-
-		unsigned long int sum = bef2 + aft2;
-
-		if (sum >= l)
-		{
-			sum -= l;
-			bef1++;
-		}
-
-		unsigned long int aft_low = sum;
-		unsigned long int aft_high = bef1 + aft1;
-
-		if (aft_high >= l)
-		{
-			aft_high -= l;
-		}
-		bef = aft;
-		aft = aft_high * l + aft_low;
-		printf(",%lu", aft);
+		temp = a + b;
+		printf(", %lu", temp);
+		a = b;
+		b = temp;
 	}
 	printf("\n");
 	return (0);
