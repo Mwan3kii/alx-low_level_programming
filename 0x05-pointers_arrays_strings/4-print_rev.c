84a214ae75b2aa8a23fd17c398d9d4c204fb2119
Mwan3kii <agathawanja303@gmail.com>
2023-10-16 13:20:53 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar('\n');
+}
