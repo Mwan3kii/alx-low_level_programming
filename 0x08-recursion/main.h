669475fa38be3b1b9997105ed0c69e1d4ed29524
Mwan3kii <agathawanja303@gmail.com>
2023-10-26 10:27:48 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,7 @@ int wildcmp(char *s1, char *s2);
 int _putchar(char c);
 int sqrt_a(int a, int b);
 int prime_a(int a, int b);
+int compare_string(char *s, int left, int right);
+int _strlen_recursion(char *s);
 
 #endif
