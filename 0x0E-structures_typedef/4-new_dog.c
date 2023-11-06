055abcfcec9c19d03c9b8e7d519fe682c1ade231
Mwan3kii <agathawanja303@gmail.com>
2023-11-07 02:31:32 +0300

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,9 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	for (n = 0; name[0]; n++)
+	for (n = 0; name[n]; n++)
 		n++;
-	dog->name = malloc(sizeof(char));
+	dog->name = malloc(n * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
@@ -33,10 +33,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->age = age;
 	for (o = 0; owner[o]; o++)
 		o++;
-	dog->owner = malloc(sizeof(char));
+	dog->owner = malloc(o * sizeof(char));
 	if (dog->owner == NULL)
 	{
-		free(dog->owner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
