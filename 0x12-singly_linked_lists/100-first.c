a49648f921003db157fea3634cbf3685f4db0307
Mwan3kii <agathawanja303@gmail.com>
2023-11-16 16:17:30 +0300

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * bfor_main - function that prints before main
+ */
+void __attribute__ ((constructor)) bfor_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
