9da4ac740e95532042208cedd6f03522496925dd
Mwan3kii <agathawanja303@gmail.com>
2024-01-15 22:39:59 +0300

    dynamic with c

--- 0x18-dynamic_libraries/div.c
+++ 0x18-dynamic_libraries/div.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * divi - does division
+ * div - does division
  * @a: first interger
  * @b: second interger
  *
  * Return: division
  */
-int divi(int a, int b)
+int div(int a, int b)
 {
 	if (b != 0)
 		return (a / b);
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -24,7 +24,6 @@ char *_strstr(char *haystack, char *needle);
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
-int divi(int a, int b);
 int mod(int a, int b);
 
 #endif
